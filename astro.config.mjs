18418ffaad2a0945af1f7435980688ab970eafdb
AFTAB AHMED <sahiljnvb@gmail.com>
2026-09-05 19:19:03 +0530

    Update astro base URL and add deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sahiljnvb.github.io',
-  base: '/daily-diary-2026',
+  base: '/daily-diary-2026/',
   output: 'static',
 });
